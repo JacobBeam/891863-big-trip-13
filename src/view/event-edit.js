972f0ebdc250417ae6ad2ba9927f2225a7a13b68
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2020-11-24 01:00:49 +0300

    правки изображений

--- src/view/event-edit.js
+++ src/view/event-edit.js
@@ -81,7 +81,7 @@ ${photo.map((value) => `<img class="event__photo" src=${value} alt="Event photo"
     <div class="event__type-wrapper">
       <label class="event__type  event__type-btn" for="event-type-toggle-1">
         <span class="visually-hidden">Choose event type</span>
-        <img class="event__type-icon" width="17" height="17" src="img/icons/${eventType}.png" alt="Event type icon">
+        <img class="event__type-icon" width="17" height="17" src="./img/icons/${eventType.toLowerCase()}.png" alt="Event type icon">
       </label>
       <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
